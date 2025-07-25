f81f8018034ba59e7224c68711c8a861e175400a
Conrad <coen.hasselaar@gmail.com>
2025-07-25 20:14:05 +0200

    Corrected names for header in device pool

--- src/devices_pool/TV/device_optoma/device_optoma.h
+++ src/devices_pool/TV/device_optoma/device_optoma.h
@@ -2,32 +2,32 @@
 
 // Contains definitions for the Omote remote model L-27-5KEY
 
-extern uint16_t BLURAY_EJECT;
-extern uint16_t BLURAY_SOURCE;
-extern uint16_t BLURAY_TV_POWER;
-extern uint16_t BLURAY_POWER;
-extern uint16_t BLURAY_RED;
-extern uint16_t BLURAY_GREEN;
-extern uint16_t BLURAY_YELLOW;
-extern uint16_t BLURAY_BLUE;
-extern uint16_t BLURAY_TOP_MENU;
-extern uint16_t BLURAY_POP_UP;
-extern uint16_t BLURAY_UP;
-extern uint16_t BLURAY_LEFT;
-extern uint16_t BLURAY_CENTER;
-extern uint16_t BLURAY_RIGHT;
-extern uint16_t BLURAY_DOWN;
-extern uint16_t BLURAY_RETURN;
-extern uint16_t BLURAY_OPTIONS;
-extern uint16_t BLURAY_HOME;
-extern uint16_t BLURAY_LIKE;
-extern uint16_t BLURAY_NETFLIX;
-extern uint16_t BLURAY_REWIND;
-extern uint16_t BLURAY_PLAY;
-extern uint16_t BLURAY_FORWARD;
-extern uint16_t BLURAY_PREVIOUS;
-extern uint16_t BLURAY_PAUSE;
-extern uint16_t BLURAY_STOP;
+extern uint16_t OPTOMA_ON;
+extern uint16_t OPTOMA_OFF;
+extern uint16_t OPTOMA_USER1;
+extern uint16_t OPTOMA_USER2;
+extern uint16_t OPTOMA_USER3;
+extern uint16_t OPTOMA_BRIGHTNESS;
+extern uint16_t OPTOMA_CONTRAST;
+extern uint16_t OPTOMA_MODE;
+extern uint16_t OPTOMA_KEYSTONE;
+extern uint16_t OPTOMA_ASPECTRATIO;
+extern uint16_t OPTOMA_3D;
+extern uint16_t OPTOMA_MUTE;
+extern uint16_t OPTOMA_DB;
+extern uint16_t OPTOMA_SLEEPTIMER;
+extern uint16_t OPTOMA_UP;
+extern uint16_t OPTOMA_LEFT;
+extern uint16_t OPTOMA_ENTER;
+extern uint16_t OPTOMA_RIGHT;
+extern uint16_t OPTOMA_DOWN;
+extern uint16_t OPTOMA_MENU;
+extern uint16_t OPTOMA_HDMI1;
+extern uint16_t OPTOMA_HDMI2;
+extern uint16_t OPTOMA_VGA1;
+extern uint16_t OPTOMA_VGA2;
+extern uint16_t OPTOMA_VIDEO;
+extern uint16_t OPTOMA_COMPONENT;
 
 void register_device_optoma();
 void turn_optoma_off();
